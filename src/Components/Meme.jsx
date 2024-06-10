24612f3c9e52b9770cbf42d4d452b46187abb0f1
saeedhalabi <saeedhalabi80@gmail.com>
2024-06-10 15:54:59 +0300

    Add meme form and data structure
    - Implemented a form with input fields for top and bottom text
    
    - Added a button to fetch a new meme image
    
    - Included a data structure containing a list of popular meme templates with their respective details (id, title, imageUrl, and description)

--- src/Components/Meme.jsx
+++ src/Components/Meme.jsx
@@ -3,11 +3,11 @@ import React from "react";
 const Meme = () => {
   return (
     <main>
-      <form className="form">
-        <input type="text" className="form--input" placeholder="Top Text" />
-        <input type="text" className="form--input" placeholder="Bottom Text" />
-        <button className="form--button">Get a new meme image 🖼</button>
-      </form>
+      <div className="form">
+        <input type="text" className="div--input" placeholder="Top Text" />
+        <input type="text" className="div--input" placeholder="Bottom Text" />
+        <button className="div--button">Get a new meme image 🖼</button>
+      </div>
     </main>
   );
 };
