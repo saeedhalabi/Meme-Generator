75d659f5484098e2d1c60928600ab33f53c95a39
saeedhalabi <saeedhalabi80@gmail.com>
2024-06-10 11:56:24 +0300

    header component

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import MemeImage from "../images/troll-face.png";
 const Header = () => {
   return (
-    <div className="header--content">
+    <header className="header--content">
       <img src={MemeImage} alt="Famous Meme Icon" />
       <h1>Meme Generator</h1>
-    </div>
+    </header>
   );
 };
 
