2b3c5ef5c389e57ca1919f6540eaba72ca87aece
saeedhalabi <saeedhalabi80@gmail.com>
2024-06-10 17:04:13 +0300

    Refactoring

--- src/Components/Meme.jsx
+++ src/Components/Meme.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import MemesData from "../memesData";
 
 const Meme = () => {
   return (
@@ -6,7 +7,7 @@ const Meme = () => {
       <div className="form">
         <input type="text" className="form--input" placeholder="Top Text" />
         <input type="text" className="form--input" placeholder="Bottom Text" />
-        <button className="form--button">Get a new meme image 🖼</button>
+        <button className="form--button">Get a new meme image 📷</button>
       </div>
     </main>
   );
