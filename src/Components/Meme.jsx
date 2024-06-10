22953e873c84d1e920ae85d3612e7363e002089a
saeedhalabi <saeedhalabi80@gmail.com>
2024-06-10 20:18:59 +0300

    style: Update styling for meme image and centering
    - Update the CSS for the meme image to ensure it fills the container but does not exceed a maximum width of 550px.
    - Center the meme image vertically and horizontally on the page with a margin of 100px.
    - Apply a border-radius of 10px to the meme image for a rounded corner effect.

--- src/Components/Meme.jsx
+++ src/Components/Meme.jsx
@@ -1,16 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import MemesData from "../memesData";
 
+// Component for rendering a meme image with top and bottom text
 const Meme = () => {
+  // State for storing the URL of the meme image
+  const [memeImage, setMemeImage] = useState("");
+
+  // Function to get a random meme image from MemesData
+  const getMemeImage = () => {
+    // Retrieve the array of meme objects from MemesData
+    const memesArray = MemesData.data.memes;
+    // Generate a random index within the length of the memesArray
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+
+    // Set the memeImage state to the URL of the randomly selected meme
+    setMemeImage(memesArray[randomNumber].url);
+  };
+
+  // Render the component
   return (
     <main>
+      {/* Form for entering top and bottom text */}
       <div className="form">
         <input type="text" className="form--input" placeholder="Top Text" />
         <input type="text" className="form--input" placeholder="Bottom Text" />
-        <button className="form--button">Get a new meme image 📷</button>
+        <button className="form--button" onClick={getMemeImage}>
+          Get a new meme image 📷
+        </button>
       </div>
+      {/* Display the meme image if memeImage state is not empty */}
+      {memeImage && (
+        <img src={memeImage} alt="Meme Image" className="meme--image" />
+      )}
     </main>
   );
 };
 
+// Export the Meme component
 export default Meme;
