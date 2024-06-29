27888c556067b7bcf18a65b36c28279c945d6b40
saeedhalabi <saeedhalabi80@gmail.com>
2024-06-29 11:23:32 +0300

    Finalize Project

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import React from "react";
 import Header from "./Components/Header";
 import Meme from "./Components/Meme";
+
 const App = () => {
   return (
     <div>
--- src/Components/Meme.jsx
+++ src/Components/Meme.jsx
@@ -1,37 +1,74 @@
-import React, { useState } from "react";
-import MemesData from "../memesData";
+import React, { useState, useEffect } from "react";
 
 // Component for rendering a meme image with top and bottom text
 const Meme = () => {
   // State for storing the URL of the meme image
-  const [memeImage, setMemeImage] = useState("");
+  const [meme, setMeme] = useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "https://i.imgflip.com/1bij.jpg",
+  });
+
+  const [allMemes, setallMemes] = useState([]);
+
+  // Fetch Memes
+  useEffect(() => {
+    fetch("https://api.imgflip.com/get_memes")
+      .then(res => res.json())
+      .then(data => setallMemes(data.data.memes));
+  }, []);
 
   // Function to get a random meme image from MemesData
   const getMemeImage = () => {
-    // Retrieve the array of meme objects from MemesData
-    const memesArray = MemesData.data.memes;
     // Generate a random index within the length of the memesArray
-    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    const randomNumber = Math.floor(Math.random() * allMemes.length);
+    const url = allMemes[randomNumber].url;
 
-    // Set the memeImage state to the URL of the randomly selected meme
-    setMemeImage(memesArray[randomNumber].url);
+    setMeme(prevMeme => ({
+      ...prevMeme,
+      randomImage: url,
+    }));
   };
+
+  // change handling function for the inputs
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setMeme(prevMeme => ({
+      ...prevMeme,
+      [name]: value,
+    }));
+  }
 
   // Render the component
   return (
     <main>
       {/* Form for entering top and bottom text */}
       <div className="form">
-        <input type="text" className="form--input" placeholder="Top Text" />
-        <input type="text" className="form--input" placeholder="Bottom Text" />
+        <input
+          type="text"
+          className="form--input"
+          placeholder="Top Text"
+          name="topText"
+          value={meme.topText}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          className="form--input"
+          placeholder="Bottom Text"
+          name="bottomText"
+          value={meme.bottomText}
+          onChange={handleChange}
+        />
         <button className="form--button" onClick={getMemeImage}>
           Get a new meme image 📷
         </button>
       </div>
-      {/* Display the meme image if memeImage state is not empty */}
-      {memeImage && (
-        <img src={memeImage} alt="Meme Image" className="meme--image" />
-      )}
+      <div className="meme">
+        <img src={meme.randomImage} alt="Meme Image" className="meme--image" />
+        <h2 className="meme--text top">{meme.topText}</h2>
+        <h2 className="meme--text bottom">{meme.bottomText}</h2>
+      </div>
     </main>
   );
 };
